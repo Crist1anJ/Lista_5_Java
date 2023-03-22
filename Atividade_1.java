013b6a35831b07ab0953594652ce03b95012bc28
Cristian Gluchak Colaços <106543901+Crist1anJ@users.noreply.github.com>
2023-03-22 20:15:28 -0300

    Update Atividade_1.java

--- Atividade_1.java
+++ Atividade_1.java
@@ -1,8 +1,8 @@
 
 public class Atividade_1 {
     public static void main (String [] args ) throws Exception {
-        double [ ] array = { 32, 10, 50, 2, 20, 15, 12, 28, 15, 45};
-        double arrayy [] = { 9,8,7,6,5,4,3,2,1,0};
+        double [ ] array = { 32, 10, 50, 2, 20, 15, 12, 28, 15, 45,11,12};
+        double arrayy [] = { 12,10,9,8,7,6,5,4,3,2,1,0};
         System.out.printf("%s%8s\n","Índice","Valor");
         for (int a = 0; a < array.length; a++)
         System.out.printf("%d\t%f\n", a, array[a]);
